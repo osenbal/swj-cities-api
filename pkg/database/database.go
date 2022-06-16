35d8c7cff7f1f7d48e1a7969af93e62254fe43ec
unknown <shaesaryan.firmansyah21@mhs.uinjkt.ac.id>
2022-06-16 23:09:03 +0700

    add connection to postgres

---
+++ pkg/database/database.go
@@ -0,0 +1,23 @@
+package database
+
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+)
+
+var (
+	db *gorm.DB
+)
+
+func Connect() {
+	connection, err := gorm.Open("postgres", "host=localhost port=5432 user=postgres dbname=swj-cities-api sslmode=disable password=VINIX")
+	if err != nil {
+		panic(err)
+	}
+
+	db = connection
+}
+
+func GetDBConnection() *gorm.DB {
+	return db
+}
