479c59f78a8a839b98d83b49d4dc9dab6360c5f4
osenbal <enbal.blogs.215@gmail.com>
2022-06-17 00:25:45 +0700

    fix routers

---
+++ pkg/routes/api.go
@@ -0,0 +1,20 @@
+package routes
+
+import (
+	"github.com/labstack/echo"
+	"gitlab.com/osenbal_slowly/swj-cities-api.git/pkg/controllers"
+)
+
+// API Lists
+var CreateCitiesRouters = func(route *echo.Echo) {
+	route.GET("/api/cities", controllers.GetAllCity)
+	route.GET("/api/city/:id", controllers.GetCityById)
+	route.GET("/api/city/search/:name", controllers.SearchByName)
+
+	route.POST("/api/city", controllers.CreateCity)
+	route.PUT("/api/city/:id", controllers.UpdateCity)
+	route.PUT("/api/city/restore/:id", controllers.RestoreData)
+
+	route.DELETE("/api/city/delete/:id", controllers.DeleteCity)
+	route.DELETE("/api/city/permanent-delete/:id", controllers.PermanentDelete)
+}
