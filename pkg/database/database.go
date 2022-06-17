a7422f58a08977486689108dee00ea1382ad8d93
osenbal <enbal.blogs.215@gmail.com>
2022-06-17 22:23:15 +0700

    make it better

--- pkg/database/database.go
+++ pkg/database/database.go
@@ -10,7 +10,7 @@ var (
 )
 
 func Connect() {
-	connection, err := gorm.Open("postgres", "host=localhost port=5432 user=postgres dbname=swj-cities-api sslmode=disable password=alumni010")
+	connection, err := gorm.Open("postgres", "host=ec2-3-224-8-189.compute-1.amazonaws.com port=5432 user=nlbthlohzvexux dbname=d9afno0o6aoc9l sslmode=disable password=5c48088c7cfb36fe27a3b53d9acfa2e5a60c590e927c831ffde73fc85a94cb96")
 	if err != nil {
 		panic(err)
 	}
