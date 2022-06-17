c467a7c4258ccd668bfece1b224f84f19647029b
osenbal <enbal.blogs.215@gmail.com>
2022-06-17 23:20:27 +0700

    Deploying to Heroku

--- pkg/database/database.go
+++ pkg/database/database.go
@@ -10,7 +10,7 @@ var (
 )
 
 func Connect() {
-	connection, err := gorm.Open("postgres", "host=ec2-34-225-159-178.compute-1.amazonaws.com port=5432 user=ywrabpnbrxmidb dbname=dde6pk2h71cob5 sslmode=disable password=1460639e3c459b49943a9447bb3e6fd0ff3e23a901f9d046fd3e4cfd895c5283")
+	connection, err := gorm.Open("postgres", "host=https://swj-cities-api.herokuapp.com port=5432 user=ywrabpnbrxmidb dbname=dde6pk2h71cob5 sslmode=disable password=1460639e3c459b49943a9447bb3e6fd0ff3e23a901f9d046fd3e4cfd895c5283")
 	if err != nil {
 		panic(err)
 	}
