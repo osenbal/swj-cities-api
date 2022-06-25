be3be6addad57d599da55ddf7dd5b30e63e419e2
osenbal <enbal.blogs.215@gmail.com>
2022-06-25 07:49:15 +0700

    merge with all division

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,15 +1,26 @@
 package main
 
 import (
-	"gitlab.com/osenbal_slowly/swj-cities-api.git/pkg/routes"
 	"net/http"
 	"os"
+
+	"gitlab.com/osenbal_slowly/swj-cities-api.git/pkg/routes"
 )
 
 func main() {
+
 	e := routes.CreateCitiesRouters()
-	http.ListenAndServe(":"+os.Getenv("PORT"), e)
+
+	if value, ok := os.LookupEnv("PORT"); ok {
+		println(value)
+		http.ListenAndServe(":"+os.Getenv("PORT"), e)
+	} else {
+		e.Start(":8080")
+	}
+
+	// if err != nil {
+	// 	fmt.Println("SERVER RUNNING ON LOCAL")
+	// }
 
 	//local
-	//e.Start(":8080")
 }
